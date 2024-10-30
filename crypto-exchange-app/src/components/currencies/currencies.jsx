dfa3ad08d0a622a28a6dac556b4542c7262f5564
Miguel Urena <urena_miguel82@outlook.com>
2024-10-30 15:02:30 -0700

    Currencies

---
+++ crypto-exchange-app/src/components/currencies/currencies.jsx
@@ -0,0 +1,24 @@
+export const cryptocurrencies = [
+  { value: "BTC", label: "Bitcoin" },
+  { value: "ETH", label: "Ethereum" },
+  { value: "BCH", label: "Bitcoin Cash" },
+  { value: "XRP", label: "Ripple" },
+  { value: "SOL", label: "Solana" },
+  { value: "ADA", label: "Cardano" },
+  { value: "BNB", label: "Binance Coin" },
+];
+
+export const fiatCurrencies = [
+  { value: "USD", label: "US Dollar" },
+  { value: "EUR", label: "Euro" },
+  { value: "GBP", label: "Pound Sterling" },
+  { value: "JPY", label: "Yen" },
+  { value: "AUD", label: "Australian Dollar" },
+  { value: "CAD", label: "Canadian Dollar" },
+  { value: "CHF", label: "Swiss Franc" },
+  { value: "CNY", label: "Chinese Yuan" },
+  { value: "NGN", label: "Naira" },
+  { value: "RUB", label: "Russian Ruble" },
+  { value: "SGD", label: "Singaporean Dollar" },
+  { value: "TRY", label: "Turkish Lira" },
+];
